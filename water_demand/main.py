9c79447a0db79f2450f7217c15d10db1b4e172ac
Fergus Cooper <fcooper8472@gmail.com>
2019-06-03 14:19:01 +0100

    Add documentation to class and methods

--- water_demand/main.py
+++ water_demand/main.py
@@ -5,12 +5,29 @@ import numpy as np
 
 
 class WaterDemand:
+    """
+    Simulate water demand based on a linear relationship between
+    population size and water demand.  This class is constructed with
+    a sequence representing populations at arbitrary nodes (e.g. LADs)
+    and a number or sequence of numbers by which to scale those
+    populations.
+    """
 
     def __init__(
             self,
             population: Sequence[Number],
             scale_factor: Union[Number, Sequence[Number]],
     ):
+        """
+        Create a WaterDemand object with given population and scale.
+
+        :param population: A sequence of numbers representing the
+          population at each of a number of nodes, e.g. LADs
+        :param scale_factor: A single number, or a sequence of numbers
+          the same length as the population, representing the linear
+          relationship between population and demand at each population
+          node.
+        """
         self._population = np.asarray(population)
 
         try:
