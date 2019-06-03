f4b4a34d2f131065f0f853650bb19af1d2291f82
Fergus Cooper <fcooper8472@gmail.com>
2019-06-03 15:00:10 +0100

    Bump version to 1.0.0

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,5 +2,5 @@ import water_demand
 
 
 def test_version():
-    assert water_demand.version() == (0, 0, 1)
-    assert(water_demand.version(formatted=True) == 'water_demand v0.0.1')
+    assert water_demand.version() == (1, 0, 0)
+    assert(water_demand.version(formatted=True) == 'water_demand v1.0.0')
